74599a7c5d8c8046d3e675b0a1ddc9e122d2d167
Dilara <dilaraismn@gmail.com>
2023-01-02 17:29:01 +0300

    Avatar and animation 01

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -7,18 +7,44 @@ using Fusion;
 public class Player : NetworkBehaviour
 {
     private NetworkCharacterControllerPrototype _characterControllerPrototype;
+    private Animator _animator;
+    private bool isWalking;
 
     private void Awake()
     {
         _characterControllerPrototype = GetComponent<NetworkCharacterControllerPrototype>();
+        _animator = GetComponentInChildren<Animator>();
+        isWalking = false;
     }
-
+    
     public override void FixedUpdateNetwork()
     {
         if (GetInput(out NetworkInputData data))
         {
             data.direction.Normalize();
             _characterControllerPrototype.Move(5 * data.direction * Runner.DeltaTime);
+            isWalking = true;
         }
+    }
+
+    private void Update()
+    {
+        if (isWalking)
+        {
+            OnWalkChanged(default);
+        }
+        else
+        {
+            OnIdleChanged(default);
+        }
+    }
+
+    public static void OnWalkChanged(Changed<Player> changed)
+    {
+        changed.Behaviour._animator.Play("Walk");
+    }
+    public static void OnIdleChanged(Changed<Player> changed)
+    {
+        changed.Behaviour._animator.Play("Idle");
     }
 }
